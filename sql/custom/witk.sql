2cbb45c7e8b3d184a55b2ba22d25b15838b468db
Exxenoz <titan.andreas@gmail.com>
2015-09-13 04:25:52 +0200

    Added WitK sql data

---
+++ sql/custom/witk.sql
@@ -0,0 +1,104 @@
+﻿/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: witk_scriptdev2
+Target Host: localhost
+Target Database: witk_scriptdev2
+Date: 13.09.2015 04:24:43
+*/
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `custom_texts` VALUES ('-2006000', 'Who is the Killer startet in %u...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006001', 'Das Spiel beginnt...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006002', 'Du hast keinen freien Platz in deinem Rucksack!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006003', 'Du hast %u Goldmünzen bekommen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006004', 'Du hast %u Eventmünzen bekommen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006005', 'Du hast 1 Goldmünze bekommen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006006', 'Du hast 1 Eventmünze bekommen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006007', '%s ist gekillt worden...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006008', '%s ist besiegt worden...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006009', 'Du hast die Spielzone verlassen. Du solltest umkehren, oder du wirst in %u Sek. teleportiert.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006010', 'Du bist ein Killer, beseitige diese nervigen Camper!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006011', 'Benutze den Befehl \".fangen\" um einen angeklickten Camper zu fangen, und danach den Befehl \".killen\" um ihn zu ermorden.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006012', 'Du musst warten, bis alle Camper abgestimmt haben.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006013', 'Du wurdest erkannt!! Laufe zum Teleportfelsen (Siehe Kartenmarkierung), um zu entkommen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006014', 'Wenn du diesen Sound hörst, dann hast du ein neues Signal von einem Camper auf der Karte bekommen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006015', 'Verwirre deine Opfer, sodass sie glauben du bist einer von ihnen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006016', 'Benutze den Befehl \".verwirren\" um die Camper mit deinem Tot zu verwirren, oder den Befehl \".wechseln\" um dich als Camper zu tarnen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006017', 'Du hast %u Sek. Zeit zum Abstimmen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006018', 'Du hast den falschen Spieler für einen Killer gehalten! Damit bist du aus dem Spiel ausgeschieden.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006019', 'Du hast einen Killer erkannt! Er ist jetzt auf dem Weg zum Teleportfelsen (Siehe Kartemarkierung), du musst ihn aufhalten!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006020', 'Du bist ein Camper, überlebe bis die Zeit abgelaufen ist, und finde heraus wer ein Killer ist.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006021', 'Wenn du die gruselige Musik hörst, ist ein Killer nicht weit entfernt von dir.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006022', 'Killer sind unsichtbar, können aber die Gestalt von einem Camper annehmen, lass dich nicht täuschen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006023', 'Alle Camper wurden besiegt, nicht schlecht! Du hast gewonnen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006024', 'Alle Killer sind offline, das Spiel wurde abgebrochen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006025', 'Du konntest durch das Portal entkommen! Du hast gewonnen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006026', 'Alle Killer sind tot! Du hast gewonnen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006027', 'Es sind nicht mehr genug Camper online, das Spiel wurde abgebrochen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006028', 'Kein Camper hat erkannt wer du wirklich bist. Du hast gewonnen!', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006029', 'Ein Killer hat dich besiegt...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006030', 'Ein Killer konnte durch das Portal entkommen, du hast leider verloren...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006031', 'Die Camper haben dich umgebracht, du hast leider verloren...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006032', 'Das Spiel endet unentschieden.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006033', 'Kann nicht verwendet werden', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006034', 'Du musst zuerst einen Killer anklicken', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006035', 'Du musst zuerst einen Camper anklicken', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006036', 'Ein Killer hat es vor dir durch das Portal geschafft, du hast leider verloren.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006037', 'Der angeklickte Spieler ist kein Camper', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006038', 'Der Killer ist zu weit entfernt', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006039', 'Dieser Camper ist zu weit entfernt', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006040', 'Dieser Camper muss zuerst gefangen werden', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006041', 'Dieser Camper wurde bereits gefangen', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006042', 'Dein Fake Camper wurde bereits besiegt', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006043', 'Du musst zuerst in der Killerform sein', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006044', 'Du kannst den Camper in %u Sekunden killen', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006045', 'Du kannst den Camper in 1 Sekunde killen', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006046', 'Du kannst den Camper jetzt killen', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006047', 'Du hast bereits einen Camper gefangen, töte zuerst diesen.', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006048', 'Das Spiel wird in %u Sekunden geschlossen...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006049', 'Das Spiel wird in 1 Sekunde geschlossen...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006050', 'Das Spiel wird in %u Minuten geschlossen...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006051', 'Das Spiel wird in 1 Minute geschlossen...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006052', 'Das Spiel wird jetzt geschlossen...', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006053', '#%u %s', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006054', '#%u %s [Kicken]', null, null, null, null, null, null, null, null, '0', '0', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006055', 'Der von dir eingegebene Spielname ist zu lang.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006056', 'Es wurden schon zuviele Spiele erstellt.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006057', 'Das Spiel wurde bereits gestartet.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006058', 'Das Spiel ist voll, es kann niemand mehr beitreten.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006059', 'Das Spiel konnte nicht gestartet werden, weil gerade ein anderes Spiel läuft.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006060', 'Das Spiel konnte nicht gestartet werden, weil zuwenige Spieler beigetreten sind.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006061', 'Du hast nicht die Rechte das zu tun.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006062', 'Du kannst nicht abstimmen, weil du der Killer bist.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006063', 'Du bist bei diesem Spiel nicht mehr angemeldet.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006064', 'Du hast abgestimmt!', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `custom_texts` VALUES ('-2006065', 'Dieses Spiel existiert nicht mehr.', null, null, null, null, null, null, null, null, '0', '4', '0', '0', null);
+INSERT INTO `gossip_texts` VALUES ('-3990300', 'Spiel Erstellen', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990301', 'Spiel Beitreten', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990302', 'Schließen', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990303', '[Leiter][Spiel Starten]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990304', '[GM][Spiel Starten]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990305', '[Aktualisieren]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990306', '[Leiter][Spiel Entfernen]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990307', '[GM][Spiel Entfernen]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990308', '[Spiel Verlassen]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990309', 'Tippe den Namen des Spiels in das kommende Feld, um das Spiel zu Erstellen.', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990310', '[Leiter][Leben: %u][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990311', '[GM][Leben: %u][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990312', '[Leiter][Killer: %u][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990313', '[GM][Killer: %u][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990314', '[Leben %u]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990315', '[%u Leben]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990316', '[Killer %u]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990317', '[%u Killer]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990318', '[Killen %u Minuten]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990319', '[%u Minuten Killen]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990320', '[Flucht %u Minuten]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990321', '[%u Minuten Flucht]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990322', '[Leiter][Killen: %u Minuten][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990323', '[GM][Killen: %u Minuten][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990324', '[Leiter][Flucht: %u Minuten][Ändern]', null, null, null, null, null, null, null, null, null);
+INSERT INTO `gossip_texts` VALUES ('-3990325', '[GM][Flucht: %u Minuten][Ändern]', null, null, null, null, null, null, null, null, null);
